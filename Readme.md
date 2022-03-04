cadb52f07f1494046e831154387a242cedd84b9a
mylamour <mylamour@163.com>
2022-03-04 10:18:57 +0800

    formatter shell script and optimized start script. add init check, remove sudo in commands at begining

--- Readme.md
+++ Readme.md
@@ -1,8 +1,10 @@
-# How to
+# SetUp
+
+## How to
 
 ## install
 1. terraform
-2. ansible
+2. ansible [optional]
 
 ```bash
 python3 -m venv runenv
@@ -14,10 +16,23 @@ pip3 install ansible awscli
 * AWS AK
 
 ## useage
+1. create instances
+use `terraform apply` or `./start.sh -m init`
+
+2. exec command to remote
+`./start.sh -m shell ls -al`
+
+3. exec tgbot
+`./start.sh -m tgbot`
+
+4. install tools
+`./start.sh -m tools `
+
 ```bash
 cd up
-./start.sh -r init
-./start.sh -r runtime docker
-./start.sh -r shell ls -al
+./start.sh
+./start.sh -m init
+./start.sh -m runtime docker
+./start.sh -m shell ls -al
 ./start.sh -s
 ```
--- up/start.sh
+++ up/start.sh
@@ -1,114 +1,128 @@
 #!/bin/bash
 
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-YELLOW=`tput setaf 3`
-NC=`tput sgr0`
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+NC=$(tput sgr0)
+DIR="$(cd "$(dirname "$0")" && pwd)"
+CONFIGDIR="$DIR/configs"
 
-usage()
-{
-  (echo "Usage: $0 [options]"
-   echo "Available options:"
-   echo "  --help                       prints this"
-   echo "  --destroy                    destroy infra"
-   echo "  -s, --shell                  enter shell"
-   echo "  -m, --module XXX             work with module"
-   ) 1>&2
+usage() {
+  (
+    echo "Usage: $0 [options]"
+    echo "Available options:"
+    echo "  --help                       prints this"
+    echo "  --destroy                    destroy infra"
+    echo "  -s, --shell                  enter shell"
+    echo "  -m, --module XXX             work with module"
+  ) 1>&2
 }
 
-destroyit(){
-    echo "${RED}[Warning]${NC}: Start to Detroy the Infra"
-    terraform destroy -auto-approve >> .logs
+destroyit() {
+  echo "${RED}[Warning]${NC}: Start to Detroy the Infra"
+  terraform destroy -auto-approve >>.logs
 }
 
-if [ -z ${1} ]
-then
+sshtoremote() {
+  oldip=$(cat terraform.tfstate | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -n 1)
+  if [ -z $oldip ]; then
+    echo "${RED}[Error]${NC}: Your Instances was not exists, Please create it first."
+    exit 1
+  else
+    ssh -o StrictHostKeyChecking=no -l ubuntu $oldip
+  fi
+}
+
+if [ -z ${1} ]; then
   usage
   exit 1
 fi
 
-while [ $# -gt 0 ]
-do
+while [ $# -gt 0 ]; do
   case $1 in
 
-    --help)
+  --help)
+    usage
+    exit 0
+    ;;
+
+  --destroy)
+    destroyit
+    exit 0
+    ;;
+
+  -s | -shell)
+    sshtoremote
+    exit 0
+    ;;
+
+  -m | --module)
+    if [ $# -lt 2 ]; then
       usage
-      exit 0
-      ;;
-        
-    --destroy)
-      destroyit
-      exit 0
-      ;;
-      
-    -s|-shell)
-      oldip=$(cat .logs | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | tail -n 1)
-      ssh -o StrictHostKeyChecking=no -l ubuntu $oldip
-      exit 0
-      ;;
+      exit 1
+    fi
 
-    -m|--module)
-      if [ $# -lt 2 ]; then
-        usage
-        exit 1
-      fi
+    modfile="${2}"
+    otherargs="${*:3}"
 
-      modfile="${2}"
-      otherargs="${*:3}"
+    shift 1
+    ;;
 
-      shift 1
-      ;;
+  # -t|--team)
+  #   if [ $# -lt 2 ]; then
+  #     usage
+  #     exit 1
+  #   fi
+  #   team="${2}"
+  #   shift 1
+  #   ;;
 
-    # -t|--team)
-    #   if [ $# -lt 2 ]; then
-    #     usage
-    #     exit 1
-    #   fi
-    #   team="${2}"
-    #   shift 1
-    #   ;;
-
-    *)
+  *) ;;
   esac
   shift 1
 done
 
-DIR="$( cd "$( dirname "$0" )" && pwd )" 
-CONFIGDIR="$DIR/configs"
 
-grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' terraform.tfstate > /dev/null
 # check whether if the instances was exists
+grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' terraform.tfstate >/dev/null
 if [ $? -eq 0 ]; then
-    echo "${GREEN}[Info]${NC}: Instances Exists"
-    # :  # pass
+  # echo "${GREEN}[Info]${NC}: Instances Exists"
+  :  # pass
 else
-    echo "${YELLOW}[Info]${NC}: Start to Create Instances"
-    terraform apply  -auto-approve >> .logs
-    
-    if [ $? -eq 0 ]; then
-        echo "${GREEN}[Info]${NC}: Instances Created"
-    else
-        echo "${RED}[ERROR]${NC}: Instances Created FAILED, Please Check it now"
-        exit 1
-    fi
 
+  # only allowed to use init to create instance
+  if [ ${modfile} != "init" ]; then
+    echo "${RED}[Error]${NC}: Your Instances was not exists, Please use './start.sh -m init' to create instance"
+    exit 0
+  else
+    echo "${YELLOW}[Info]${NC}: Start to Create Instances"
+    terraform apply -auto-approve >>.logs
+
+    if [ $? -eq 0 ]; then
+      echo "${GREEN}[Info]${NC}: Instances Created"
+      exit 0
+    else
+      echo "${RED}[ERROR]${NC}: Instances Created FAILED, Please check it now"
+      exit 1
+    fi
+  fi
 fi
 
-if [ -f $CONFIGDIR/${modfile} ]
-then
-    echo "${GREEN}[Info]${NC}: Start to building with module: ${modfile}"
-    
-    newip=$(cat .logs | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | tail -n 1)
+# if instances was exists then was able to load modules
+if [ -f $CONFIGDIR/${modfile} ]; then
+  echo "${GREEN}[Info]${NC}: Start to building with module: ${modfile}"
 
-    rsync -e "ssh -o StrictHostKeyChecking=no" $CONFIGDIR/${modfile} ubuntu@${newip}:/tmp/
-    # ansible all -i $newip, -m ansible.builtin.script -a "$CONFIGDIR/${modfile} ${otherargs}" --user=ubuntu
-   
-    command="sudo nohup bash /tmp/${modfile} ${otherargs} &"
-    ssh -o StrictHostKeyChecking=no -o LogLevel=quiet ubuntu@${newip} ${command}
+  newip=$(cat .logs | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | tail -n 1)
 
-    # ansible all -i $newip, -m shell -a "${command}" --user=ubuntu 
-    # ssh -o StrictHostKeyChecking=no -l ubuntu $newip
+  rsync -e "ssh -o StrictHostKeyChecking=no" $CONFIGDIR/${modfile} ubuntu@${newip}:/tmp/
+  # ansible all -i $newip, -m ansible.builtin.script -a "$CONFIGDIR/${modfile} ${otherargs}" --user=ubuntu
+
+  command="nohup bash /tmp/${modfile} ${otherargs} &"
+  ssh -o StrictHostKeyChecking=no -o LogLevel=quiet ubuntu@${newip} ${command}
+
+  # ansible all -i $newip, -m shell -a "${command}" --user=ubuntu
+  # ssh -o StrictHostKeyChecking=no -l ubuntu $newip
 
 else
-    echo "${RED}[Error]${NC}: Your Module was Not Exists, Please make sure it was exist in configs folder"
+  echo "${RED}[Error]${NC}: Your Module was Not Exists, Please make sure it was exist in configs folder"
 fi
